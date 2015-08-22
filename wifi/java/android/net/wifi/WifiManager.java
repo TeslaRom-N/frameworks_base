01e60a42a012e1c39a2fe87fd58d4c719ed4cfa9
William Clark <wclark@codeaurora.org>
2015-08-21 17:44:46 -0700

    SEEMP: add framework instrumentation
    - Instrument framework files to log events of interest for QSSP.
    
    - Fix the String out of boundary
    
    CRs-Fixed: 1020829
    
    Change-Id: Iab970a78e48a504a9eff066cb3c0abe83a62513a

--- wifi/java/android/net/wifi/WifiManager.java
+++ wifi/java/android/net/wifi/WifiManager.java
@@ -1319,6 +1319,7 @@ public class WifiManager {
      * returned.
      */
     public List<ScanResult> getScanResults() {
+        android.util.SeempLog.record(55);
         try {
             return mService.getScanResults(mContext.getOpPackageName());
         } catch (RemoteException e) {

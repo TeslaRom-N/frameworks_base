7afa135d23cc6d037305668ef4be6434a32a0651
Lars Greiss <kufikugel@googlemail.com>
2015-10-23 19:56:01 +0000

    Add reboot menu [1/2]
    - prepare for Global menu
    - misc changes
    
    Adding advanced reboot menu like it was on kk4.4
    Adding advanced reboot secure option
    Move reboot into safe mode from longpress power to longpress reboot
    Enable for now per default Airplane mode and sound and reboot till we have imported our SlimActions class to make in configurable
    
    Misc changes and adaptions to LP, M, and now N
    
    Change-Id: I97692bc15e3d0d9962b53b6485b2081c6d6948b3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6129,6 +6129,12 @@ public final class Settings {
                 INCALL_POWER_BUTTON_BEHAVIOR_SCREEN_OFF;
 
         /**
+         * Whether to include options in power menu for rebooting into recovery and bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Whether the device should wake when the wake gesture sensor detects motion.
          * @hide
          */
@@ -6637,6 +6643,7 @@ public final class Settings {
             CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED,
             SYSTEM_NAVIGATION_KEYS_ENABLED,
             QS_TILES,
+            ADVANCED_REBOOT,
         };
 
         /**

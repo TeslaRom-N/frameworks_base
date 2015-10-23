129032a5b3cffd2e74f1c22197bfe11f49fbb53f
Lars Greiss <kufikugel@googlemail.com>
2015-10-23 04:37:46 +0000

    Add back volume button control [1/2]
    this commit adds:
    
    - volume button control for navigate in the message - thanks cm
    
    some fixes to the 4.2/4.3 implementations + added full multiuser
    support for all this features
    
    cheers
    
    Change-Id: I71e93c39620cc171e658b2d7e26491bac97c999d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4460,6 +4460,15 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Volume keys control cursor in text fields (default is 0)
+         * 0 - Disabled
+         * 1 - Volume up/down moves cursor left/right
+         * 2 - Volume up/down moves cursor right/left
+         * @hide
+         */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Override and forcefully disable the fullscreen keyboard
          * @hide
          */

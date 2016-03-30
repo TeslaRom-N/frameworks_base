a5909658eae732cda1456bb3a015acad976edb55
Shammi Khattar <skhattar@codeaurora.org>
2016-03-30 15:46:11 -0700

    Make data call errors configurable.
    Retry requirements of carriers for ACTIVATION_REJECT_GGSN and
    PROTOCOL_ERRORS may be different and should be made configurable.
    
    CRs-Fixed: 1017540
    
    Change-Id: Id49121c9eafc32384559c7ea1bf0edf8357d0d87
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	telephony/java/android/telephony/CarrierConfigManager.java

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -901,6 +901,18 @@ public class CarrierConfigManager {
     /** @hide */
     public static final int CDMA_ROAMING_MODE_ANY = 2;
 
+     /**
+     * Key that determines if ACTIVATE_REJECT_GGSN is to be treated as a permanent error.
+     * @hide
+     */
+    public static final String KEY_REJECT_GGSN_PERM_FAILURE = "reject_ggsn_perm_failure";
+
+    /**
+     * Key that determines if PROTOCOL_ERRORS is  treated as permanent error.
+     * @hide
+     */
+    public static final String KEY_PROTOCOL_ERRORS_PERM_FAILURE = "protocol_errors_perm_failure";
+
     /**
      * Report IMEI as device id even if it's a CDMA/LTE phone.
      *
@@ -1207,6 +1219,8 @@ public class CarrierConfigManager {
         sDefaults.putBoolean(KEY_CONFIG_WIFI_DISABLE_IN_ECBM, false);
         sDefaults.putBoolean(KEY_CARRIER_NAME_OVERRIDE_BOOL, false);
         sDefaults.putString(KEY_CARRIER_NAME_STRING, "");
+        sDefaults.putBoolean(KEY_REJECT_GGSN_PERM_FAILURE,true);
+        sDefaults.putBoolean(KEY_PROTOCOL_ERRORS_PERM_FAILURE,true);
 
         // MMS defaults
         sDefaults.putBoolean(KEY_MMS_ALIAS_ENABLED_BOOL, false);

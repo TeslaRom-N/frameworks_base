071ae439df600290e0ab80da0e2a93adf426811a
0xD34D <clark@scheffsblend.com>
2016-06-20 22:59:48 +0300

    N-Extras: Add dynamic theme BootAnimation support
    Extracted from "Themes: Port to CM13 [1/3]"
    http://review.cyanogenmod.org/#/c/113273/14
    
    Change-Id: I394897c10f02695f0416e87e9bf960e840bcb3b7

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -60,6 +60,8 @@
 #include "BootAnimation.h"
 #include "audioplay.h"
 
+#define THEME_BOOTANIMATION_FILE "/data/system/theme/bootanimation.zip"
+
 namespace android {
 
 static const char OEM_BOOTANIMATION_FILE[] = "/oem/media/bootanimation.zip";
@@ -308,6 +310,9 @@ status_t BootAnimation::readyToRun() {
 
     if (encryptedAnimation && (access(SYSTEM_ENCRYPTED_BOOTANIMATION_FILE, R_OK) == 0)) {
         mZipFileName = SYSTEM_ENCRYPTED_BOOTANIMATION_FILE;
+    }
+    else if (access(THEME_BOOTANIMATION_FILE, R_OK) == 0) {
+        mZipFileName = THEME_BOOTANIMATION_FILE;
     }
     else if (access(OEM_BOOTANIMATION_FILE, R_OK) == 0) {
         mZipFileName = OEM_BOOTANIMATION_FILE;

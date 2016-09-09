904223b58ca0d46bb9fb4f51c0c56b06b49746d3
Martinusbe <martinusbe@gmail.com>
2016-09-09 14:38:54 +0200

    Revert "Add broadcast and query API for AudioSource.HOTWORD."
    This reverts commit 5b9fc3773e2bc8b7b791a6c1c1e4455671022d18.

--- media/java/android/media/AudioRecord.java
+++ media/java/android/media/AudioRecord.java
@@ -994,11 +994,6 @@ public class AudioRecord implements AudioRouting
                 mRecordingState = RECORDSTATE_RECORDING;
             }
         }
-
-        if (getRecordingState() == RECORDSTATE_RECORDING &&
-                getAudioSource() == MediaRecorder.AudioSource.HOTWORD) {
-            handleHotwordInput(true);
-        }
     }
 
     /**
@@ -1041,10 +1036,6 @@ public class AudioRecord implements AudioRouting
             native_stop();
             mRecordingState = RECORDSTATE_STOPPED;
         }
-
-        if (getAudioSource() == MediaRecorder.AudioSource.HOTWORD) {
-            handleHotwordInput(false);
-        }
     }
 
     private final IBinder mICallBack = new Binder();
@@ -1058,16 +1049,6 @@ public class AudioRecord implements AudioRouting
             ias.forceRemoteSubmixFullVolume(starting, mICallBack);
         } catch (RemoteException e) {
             Log.e(TAG, "Error talking to AudioService when handling full submix volume", e);
-        }
-    }
-
-    private void handleHotwordInput(boolean listening) {
-        final IBinder b = ServiceManager.getService(android.content.Context.AUDIO_SERVICE);
-        final IAudioService ias = IAudioService.Stub.asInterface(b);
-        try {
-            ias.handleHotwordInput(listening);
-        } catch (RemoteException e) {
-            Log.e(TAG, "Error talking to AudioService when handling hotword input.", e);
         }
     }
 

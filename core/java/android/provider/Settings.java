574433696dcd1a6bbbfeabdd16d04eec988efd6b
xyyx <xyyx@mail.ru>
2016-10-05 09:06:44 +0800

    Bring back 'custom small QS tiles' [1/2]
    Change-Id: I0d5829643ab37ea76863c9efeeff64b9e2961f58

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7476,6 +7476,12 @@ public final class Settings {
         public static final String LOCKSCREEN_TARGETS = "lockscreen_target_actions";
 
         /**
+         * Count of small QS tiles.
+         * @hide
+         */
+        public static final String QQS_COUNT = "sysui_qqs_count";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear

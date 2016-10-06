1d647d00b6353e1281d14e83bde3291782a70fa6
maxwen <max.weninger@gmail.com>
2016-10-07 00:58:36 +0200

    base: battery style: fix layout changes
    react immediately to font size and density changes
    recalc layout when switching between font sizes
    for 99 vs. 100%
    
    Change-Id: Ieec30c3ff6d0029d9da4d5a6390d7535222c4db7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -177,7 +177,6 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
 
         //mBatteryMeterView.setLayoutParams(scaledLayoutParams);
         //mBatteryMeterViewKeyguard.setLayoutParams(scaledLayoutParams);
-        //mBatteryViewManager.setLayoutParams(scaledLayoutParams);
     }
 
     @Override
@@ -606,6 +605,7 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
                     ViewGroup.LayoutParams.WRAP_CONTENT, mIconSize);
             child.setLayoutParams(lp);
         }
+        mBatteryViewManager.onDensityOrFontScaleChanged();
         //scaleBatteryMeterViews(mContext);
     }
 

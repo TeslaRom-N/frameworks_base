1cc88d40307204daec9dc4af65fd70e70e0326dd
theringmaster <gzroms@hotmail.com>
2016-10-21 10:05:31 -0400

    Lockscreen weather improvementssquashed
    LSWeather: panel improvements (1/2)
    - Move weather panel to the bottom the clock panel
    - Add wind
    - Add humidity
    - Add Condition
    - Add Timestamp
    - Add condition icons (colored and VClouds)
    - Rearrange the weather panel:
      - Left side:
        - Location
        - Wind
      - Center:
        - Condiditon icon
      - Right side:
        - Current temperature
        - Humidity
      - Bottom:
        - Condition
        - Timestamp
    - Option to show/hide the timestamp
    - Option to colorize the monochrome icons
    
    Change-Id: I986975c13c5d29bd8103424e020ad775a8f03804
    
    This also includes:
    FWB: Proper LS weather switch from XXMrHyde
    AICP/frameworks_base@ff1ceb4
    
    Weather: Handle no weather information available, (1/2)
    AICP/frameworks_base@b1406dc
    
    Lock screen: Update Handle no weather information available
    AICP/frameworks_base@3691a58
    XXMrHyde
    
    make lockscreen weather fonts a bit smaller
    darkobas
    
    LSWeather: options to hide the weather when notifications are
    visible…
    … (1/2)
    
    Hide the weather when:
      - Visible notifications reach the maximum allowed notifications on
    the lock screen
        (including the notification overlay container)
      - Visible notifications reach a customizable number of notifications
      - Never
    
    Customizable number of notifications (to hide the weather panel):
      - 1 (always)
      - 2
      - 3
      - 4
      - 5
      - 6
    XXMrHyde
    
    Fix NPE with WeatherStatus on KeyguardStatusView
    tobitege
    
    Improve lockscreen weather layout.
    AlmightyMegadeth00
    
    Clean Up Lockscreen Weather (items & layout) [1/2]
    *previously it was pretty janky lockscreen colors still needs rework
    *removed values cLock doesnt natively show "wind, humidity"
    *removed redundant timestamp that just wastes space
    *changed layout of lockscreen weather a bit
    *also made things more symetrical
    
    Before: http://i.imgur.com/YNV6XBvl.png
    After: http://i.imgur.com/MYDie6tl.png
    beanstown106

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4186,6 +4186,53 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Whether to show the weather info on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER = "lock_screen_show_weather";
+
+        /**
+         * Whether to show the weather location lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER_LOCATION = "lock_screen_show_weather_location";
+
+        /**
+         * Weather condition icon on the lock screen
+         * 0: monochrome
+         * 1: colored
+         * 2: vclouds
+         * default: 0
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_CONDITION_ICON = "lock_screen_weather_condition_icon";
+
+        /**
+         * Whether to hide the weather panel when the notifications will reach the number of notifications
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_HIDE_PANEL = "lock_screen_weather_hide_panel";
+
+        /**
+         * Hide the weather panel when the visible lockscreen notifications will reach this number
+         * 1: 1 notification (always)
+         * 2: 2 notifications
+         * 3: 3 notifications
+         * 4: 4 notifications
+         * 5: 5 notifications
+         * 6: 6 notifications
+         * default: 4
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_NUMBER_OF_NOTIFICATIONS = "lock_screen_weather_number_of_notifications";
+
+        /**
+         * Number of notifications visible on the lockscreen including the overflow container
+         * @hide
+         */
+        public static final String LOCK_SCREEN_VISIBLE_NOTIFICATIONS = "lock_screen_visible_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

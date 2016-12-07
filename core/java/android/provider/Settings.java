7bb40b16e97dfdfa0137cb003a0b8988ae1e6560
cristianomatos <cristianobmatos@gmail.com>
2016-12-07 14:50:41 +0800

    Recents Clear All Button Tweaks and Add FAB to Recents Panel
    Thanks to RR and @varund7726
    
    Extracted, cleaned and updated for Nitrogen OS by @xyyx

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4317,6 +4317,18 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_CARRIER = "status_bar_show_carrier";
 
         /**
+         * show clear all recents button
+         * @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
          * Should the alarm be shown in fullscreen instead of heads-up
          *
          * @hide

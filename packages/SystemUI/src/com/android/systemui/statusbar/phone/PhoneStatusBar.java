4c07aff873905763e2a265d88fb4a2b026626f15
maxwen <max.weninger@gmail.com>
2017-01-13 01:25:10 +0100

    base: SystemUI: fix custom header dpi change behaviour
    on dpi changes the complete QuickStatusBarHeader is recreated
    so it must be readded as observer to StatusBarHeaderMachine
    
    Change-Id: I2ddbd6abb4df747af640cbb6cb05b2260b5b30d3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1368,6 +1368,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateWeatherTextState(mWeatherController.getWeatherInfo().temp,
 mWeatherTempSize, mWeatherTempFontStyle, mWeatherTempColor);
 
+        mStatusBarHeaderMachine = new StatusBarHeaderMachine(mContext);
+
         // Set up the quick settings tile panel
         AutoReinflateContainer container = (AutoReinflateContainer) mStatusBarWindow.findViewById(
                 R.id.qs_auto_reinflate_container);
@@ -1390,6 +1392,13 @@ mWeatherTempSize, mWeatherTempFontStyle, mWeatherTempColor);
                     mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
                     mKeyguardStatusBar.setQSPanel(mQSPanel);
                     mHeader = qsContainer.getHeader();
+
+                    // on dpi changes the header is recreated so we need
+                    // to add the new one again as addObserver
+                    // the old one will be removed in the same step
+                    mStatusBarHeaderMachine.addObserver((QuickStatusBarHeader) mHeader);
+                    mStatusBarHeaderMachine.updateEnablement();
+
                     initSignalCluster(mHeader);
                     mHeader.setActivityStarter(PhoneStatusBar.this);
                 }

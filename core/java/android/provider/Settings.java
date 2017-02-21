2d5fb1bbfb3e03c07d2e3e86779c12f493d76fa0
kufikugel <kufikugel@googlemail.com>
2017-02-21 03:31:28 +0100

    IME Switcher notification [1/2]
    Change-Id: I61cadd751bc63bc3953f736bb294cf79a58b53ac

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4103,6 +4103,12 @@ public final class Settings {
         public static final String IMMERSIVE_RECENTS = "immersive_recents";
 
         /**
+         * Whether to show the IME switcher in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
+
+        /**
          * Whether to display sound panel in the power menu
          *
          * @hide

e5f2147142c523a911dd68ddcd2186fa294efcae
ezio84 <brabus84@gmail.com>
2017-03-01 18:29:08 +0100

    Slim Recent: longpress then drag up/down the app to run it on multiwindow
    also hide additional header buttons when starting multiwindow from them

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentCard.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentCard.java
@@ -248,6 +248,7 @@ public class RecentCard extends Card {
                         ActivityManagerNative.getDefault()
                                 .startActivityFromRecents(mPersistentTaskId, options.toBundle());
                         mSlimRecents.openLastApptoBottom();
+                        hideCurrentOptions();
                     } catch (RemoteException e) {}
                     return; 
                 }
@@ -270,6 +271,10 @@ public class RecentCard extends Card {
         options.findViewById(R.id.market).setOnClickListener(listener);
         options.findViewById(R.id.close).setOnClickListener(listener);
         options.findViewById(R.id.multiwindow).setOnClickListener(listener);
+    }
+
+    public void hideCurrentOptions() {
+        getCardView().hideOptions(-1, -1);
     }
 
     private Intent getAppInfoIntent() {

01e5325c261b4e218a1686ef503d658561d10ffc
Sultanxda <sultanxda@gmail.com>
2017-04-15 17:43:52 -0700

    fingerprint: Speed up wake-and-unlock scenario
    * Directly dismiss keyguard instead of using a fade-out animation
    * Skip virtual navigation button animation on wake
    
    This patch is for Nougat.
    
    Change-Id: I7d479ce76526e0051b838053fdeba99dd5c86dd2
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1859,9 +1859,14 @@ public class KeyguardViewMediator extends SystemUI {
                 playSounds(false);
             }
 
+            boolean wakeAndUnlocking = mWakeAndUnlocking;
             mWakeAndUnlocking = false;
             setShowingLocked(false);
-            mStatusBarKeyguardViewManager.hide(startTime, fadeoutDuration);
+            if (wakeAndUnlocking) {
+                mStatusBarKeyguardViewManager.hideNoAnimation();
+            } else {
+                mStatusBarKeyguardViewManager.hide(startTime, fadeoutDuration);
+            }
             resetKeyguardDonePendingLocked();
             mHideAnimationRun = false;
             updateActivityLockScreenState();
